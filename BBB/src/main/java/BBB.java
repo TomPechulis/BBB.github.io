9a56bb3b41bfe02493ca167d7e74978f0eba0e0a
TomPechulis <49105747+TomPechulis@users.noreply.github.com>
2020-10-26 22:02:13 -0500

    Update BBB.java
    testing for Home

--- BBB/src/main/java/BBB.java
+++ BBB/src/main/java/BBB.java
@@ -3,20 +3,22 @@ import java.util.List;
 
 public class BBB {
     public static void main(String [] args) throws IOException {
+
         Library library = new Library();
 
         Login login = new Login();
         login.getLogin(library);
 
-        if(login.checkLogin()){
+        if (login.checkLogin()) {
             Account a = login.getAcc();
-            Profile p = library.getProfile(a);
+            Home home = new Home();
+            home.getHome(library, a);
 
-            /* tests for editing profile and updating in library
+           /* tests for editing profile and updating in library
             p.editProfile();
             library.updateProfile(p);
 
-             */
+*/
 
 
             /* tests for adding listing, editing listing, and updating in library
